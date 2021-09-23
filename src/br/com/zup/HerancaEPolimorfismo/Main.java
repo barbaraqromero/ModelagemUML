8616caa98b38517ae340894d4a68b1b89db85506
Bárbara Romero <bqromero@gmail.com>
2021-09-23 18:06:38 -0300

    Criando método para calcular salário do Colaborador e aplicando na classe principal

--- src/br/com/zup/HerancaEPolimorfismo/Main.java
+++ src/br/com/zup/HerancaEPolimorfismo/Main.java
@@ -5,12 +5,13 @@ public class Main {
     //Instanciando as classes
     Pessoa pessoa = new Pessoa();
     Fornecedor fornecedor = new Fornecedor("Ana", "Rua 1", "33333333", 2500, 1000);
+    Colaborador colaborador = new Colaborador("Bruna", "Rua 2", "4444444444", 157, 3000, 250);
 
-    //Aplicando para obter saldo do fornecedor
+    //Aplicando método para obter saldo do fornecedor
     System.out.println(fornecedor.obterSaldo());
 
-
-
+    //Aplicando método para calcular salário do colaborador
+    System.out.println(colaborador.calcularSalario());
 
 
   }
