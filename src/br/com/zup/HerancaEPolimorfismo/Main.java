792cc5a7ee6214629522ddb61f651820edefe956
Bárbara Romero <bqromero@gmail.com>
2021-09-23 18:15:39 -0300

    Criando método para calcular o salário do Administrador e exibindo na classe principal

--- src/br/com/zup/HerancaEPolimorfismo/Main.java
+++ src/br/com/zup/HerancaEPolimorfismo/Main.java
@@ -6,12 +6,16 @@ public class Main {
     Pessoa pessoa = new Pessoa();
     Fornecedor fornecedor = new Fornecedor("Ana", "Rua 1", "33333333", 2500, 1000);
     Colaborador colaborador = new Colaborador("Bruna", "Rua 2", "4444444444", 157, 3000, 250);
+    Administrador administrador = new Administrador("Camila", "Rua 3", "55555555", 155, 5000, 500, 260);
 
     //Aplicando método para obter saldo do fornecedor
     System.out.println(fornecedor.obterSaldo());
 
     //Aplicando método para calcular salário do colaborador
     System.out.println(colaborador.calcularSalario());
+
+    //Aplicando método para calcular saário do administrador, que é um colaborador
+    System.out.println(administrador.calcularSalario());
 
 
   }
