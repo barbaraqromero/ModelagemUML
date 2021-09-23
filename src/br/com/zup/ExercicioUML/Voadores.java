d2a8c9b65cde2c26cd5aa9a7a6cc516ccdab604f
Bárbara Romero <bqromero@gmail.com>
2021-09-23 12:51:35 -0300

    Criando construtor e método Aplicar Dano na subclasse Voadores

--- src/br/com/zup/ExercicioUML/Voadores.java
+++ src/br/com/zup/ExercicioUML/Voadores.java
@@ -1,4 +1,14 @@
 package br.com.zup.ExercicioUML;
 
-public class Voadores {
+public class Voadores extends Inimigos {
+  private double danoTiro;
+
+  public Voadores(String nome, double vida, double danoTiro) {
+    super(nome, vida);
+    this.danoTiro = danoTiro;
+  }
+
+  private double aplicarDano(){
+    return aplicarDano();
+  }
 }
