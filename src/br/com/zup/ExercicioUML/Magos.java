511be388730a768ffd0344f7fab8ef8f49c7940f
Bárbara Romero <bqromero@gmail.com>
2021-09-23 12:00:28 -0300

    Criando construtor e método Aplicar Magia para a subclasse Magos

--- src/br/com/zup/ExercicioUML/Magos.java
+++ src/br/com/zup/ExercicioUML/Magos.java
@@ -1,4 +1,14 @@
 package br.com.zup.ExercicioUML;
 
-public class Magos {
+public class Magos extends Heroi{
+  private double poderMagico;
+
+  public Magos(String nome, double vida, double poderMagico) {
+    super(nome, vida);
+    this.poderMagico = poderMagico;
+  }
+
+  public double aplicarMagia(){
+    return aplicarMagia();
+  }
 }
