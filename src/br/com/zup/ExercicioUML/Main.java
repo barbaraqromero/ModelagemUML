24f4a9f495fd2d1f70b18b8583504c8b7a267eeb
Bárbara Romero <bqromero@gmail.com>
2021-09-23 13:06:17 -0300

    Instanciando classes no Main

--- src/br/com/zup/ExercicioUML/Main.java
+++ src/br/com/zup/ExercicioUML/Main.java
@@ -2,7 +2,11 @@ package br.com.zup.ExercicioUML;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    Guerreiros guerreiro = new Guerreiros("Tchalla", 200, 500);
+    Magos mago = new Magos("Dr. Estranho", 600, 2000);
+
+    Rasteiros rasteiro = new Rasteiros("Thanos", 250, 280);
+    Voadores voador = new Voadores("Abutre", 500, 650);
+  }
 }
