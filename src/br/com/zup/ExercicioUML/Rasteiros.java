54ac741be616a2a8f2f6981fabc2fe87bf42acee
Bárbara Romero <bqromero@gmail.com>
2021-09-23 12:49:11 -0300

    Criando construtor e método Aplicar Dano para a subclasse Rasteiros

--- src/br/com/zup/ExercicioUML/Rasteiros.java
+++ src/br/com/zup/ExercicioUML/Rasteiros.java
@@ -1,4 +1,14 @@
 package br.com.zup.ExercicioUML;
 
-public class Rasteiros {
+public class Rasteiros extends Inimigos{
+  private double danoEspinhos;
+
+  public Rasteiros(String nome, double vida, double danoEspinhos) {
+    super(nome, vida);
+    this.danoEspinhos = danoEspinhos;
+  }
+
+  private double aplicarDano(){
+    return aplicarDano();
+  }
 }
